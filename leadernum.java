b6cbff0645a21f923f32c262899fc0564e02e907
kavikannavkl <kavikannavkl@gmail.com>
2024-02-16 19:05:48 +0530

    First commit

---
+++ leadernum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class leadernum {
+    public static void main(String[]args)
+    {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    // List<Integer> ld =new ArrayList<>();
+    for(int i=0;i<arr.length;arr[i++]=sc.nextInt());
+    // for(int i=0;i<arr.length;i++)
+    // {
+    //     if(i==0 )
+    //     {
+    //         ld.add(arr[i]);
+    //         if(arr[i]<arr[i+1]){
+    //         ld.add(arr[i+1]);}
+    //     }
+    //     else if(i+1==arr.length-1)
+    //     {
+    //         break;
+    //     }
+    //     else if(arr[i]<arr[i+1])
+    //     {
+    //         ld.add(arr[i+1]);
+    //     }
+    // }
+    // for(int i=0;i<ld.size();System.out.print(ld.get(i++ )+" "));
+    // int leader=0;
+    // for(int i=0;i<arr.lenght();i++)
+
+    }
+
+    }
+
