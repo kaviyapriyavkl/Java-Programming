1df12aa44ede1f09dd041de1862bbba13289146c
kavikannavkl <kavikannavkl@gmail.com>
2024-03-12 09:42:46 +0530

    First commit

---
+++ floatdoub.java
@@ -0,0 +1,10 @@
+public class floatdoub {
+    public static void main(String[]args)
+    {
+        float n=2.6f;
+        float m=2;
+        double o=1;
+        double p=2.6;
+        System.out.print(n+" "+m+" "+o+" "+p);
+    }
+}
